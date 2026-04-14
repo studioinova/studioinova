16abc1f498b1cde9ae6b533971484be32243c093
studioinova <s95757132@gmail.com>
2026-04-14 11:53:09 +0000

    Fixed Home.tsx children error

--- artifacts/studio-inova/src/pages/Home.tsx
+++ artifacts/studio-inova/src/pages/Home.tsx
@@ -42,6 +42,7 @@ export default function Home() {
       <section className="py-24 bg-white">
         <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <FadeIn direction="up">
+            <div></div>
           </FadeIn>
         </div>
       </section>
